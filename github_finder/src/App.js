04de9099dabd150f757ba7cd635694180ce975cd
varunswing <varuns939@gmail.com>
2020-03-17 23:56:15 +0530

    Update App.js

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
 	state = {
 		users: [],
-		loading: false
+		loading: false,
+		alert: null
 	};
 
 	//Search Github Users
@@ -22,12 +24,28 @@ class App extends Component {
 		this.setState({ users: res.data.items, loading: false });
 	};
 
+	// Clear users from state
+	clearUsers = () => this.setState({ users: [], loading: false });
+
+	// Set Alert
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg, type }});
+
+		setTimeout(() => this.setState({ alert: null }), 5000);
+	};
+
 	render() {
 		return (
 			<div className='App'>
 				<Navbar />
 				<div className='container'>
-					<Search searchUsers={this.searchUsers} />
+					<Alert alert={this.state.alert} />
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={this.state.users.length > 0 ? true : false}
+						setAlert={this.setAlert}
+					/>
 					<Users
 						loading={this.state.loading}
 						users={this.state.users}
