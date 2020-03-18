d3ab5efc2142c11de2f4403cef272cc6f4a8abc5
varunswing <varuns939@gmail.com>
2020-03-18 23:45:26 +0530

    Update App.js

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -29,29 +31,46 @@ class App extends Component {
 
 	// Set Alert
 	setAlert = (msg, type) => {
-		this.setState({ alert: { msg, type }});
+		this.setState({ alert: { msg, type } });
 
 		setTimeout(() => this.setState({ alert: null }), 5000);
 	};
 
 	render() {
+		const { users, loading } = this.state;
+
 		return (
-			<div className='App'>
-				<Navbar />
-				<div className='container'>
-					<Alert alert={this.state.alert} />
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={this.state.users.length > 0 ? true : false}
-						setAlert={this.setAlert}
-					/>
-					<Users
-						loading={this.state.loading}
-						users={this.state.users}
-					/>
+			<Router>
+				<div className='App'>
+					<Navbar />
+					<div className='container'>
+						<Alert alert={this.state.alert} />
+						<Switch>
+							<Route
+								exact
+								path='/'
+								render={props => (
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsers}
+											clearUsers={this.clearUsers}
+											showClear={
+												users.length > 0 ? true : false
+											}
+											setAlert={this.setAlert}
+										/>
+										<Users
+											loading={loading}
+											users={users}
+										/>
+									</Fragment>
+								)}
+							/>
+							<Route exact path='/about' component={About} />
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
