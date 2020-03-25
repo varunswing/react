4127677d306a98d7b098bafc87611b7b5f9b3e99
varunswing <varuns939@gmail.com>
2020-03-25 22:52:19 +0530

    Update Users.js

--- github_finder/src/components/users/Users.js
+++ github_finder/src/components/users/Users.js
@@ -1,31 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/spinner';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Users = ({ users, loading }) => {
-    if(loading){
-        return <Spinner />
-    }else{
-        return (
-            <div style={userStyle}>
-                {users.map(user => (
-                    <UserItem key={user.id} user={user} />
-                ))}
-            </div>
-        );
-    }
-}
+const Users = () => {
+	const githubContext = useContext(GithubContext);
 
-Users.proTypes = {
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired
-}
+	const { loading, users } = githubContext;
 
-const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem'
+	if (loading) {
+		return <Spinner />;
+	} else {
+		return (
+			<div style={userStyle}>
+				{users.map(user => (
+					<UserItem key={user.id} user={user} />
+				))}
+			</div>
+		);
+	}
 };
 
-export default Users
+const userStyle = {
+	display: 'grid',
+	gridTemplateColumns: 'repeat(3, 1fr)',
+	gridGap: '1rem'
+};
+
+export default Users;
