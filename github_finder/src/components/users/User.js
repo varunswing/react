a7ba928e25d28c7e5aced94cc7be29c6779b7290
varunswing <varuns939@gmail.com>
2020-03-26 23:30:11 +0530

    Update User.js

--- github_finder/src/components/users/User.js
+++ github_finder/src/components/users/User.js
@@ -2,13 +2,12 @@ import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/spinner';
 import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
 
-const User = ({ getUserRepos, repos, match }) => {
+const User = ({ match }) => {
 	const githubContext = useContext(GithubContext);
 
-	const { getUser, loading, user } = githubContext;
+	const { getUser, loading, user, repos, getUserRepos } = githubContext;
 
 	useEffect(() => {
 		getUser(match.params.login);
@@ -111,11 +110,6 @@ const User = ({ getUserRepos, repos, match }) => {
 			<Repos repos={repos} />
 		</Fragment>
 	);
-};
-
-User.propTypes = {
-	repos: PropTypes.array.isRequired,
-	getUserRepos: PropTypes.func.isRequired
 };
 
 export default User;
