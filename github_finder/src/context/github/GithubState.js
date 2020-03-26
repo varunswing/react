2dd50d8f7decbb0a3260c2bab09494c3e3ac7471
varunswing <varuns939@gmail.com>
2020-03-26 23:30:41 +0530

    Update GithubState.js

--- github_finder/src/context/github/GithubState.js
+++ github_finder/src/context/github/GithubState.js
@@ -48,7 +48,19 @@ const GithubState = props => {
         })
 	};
 
-	// Get Repos
+    // Get Repos
+    const getUserRepos = async username => {
+		setLoading();
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CIENT_ID}&client_search=${process.env.REACT_APP_GITHUB_CIENT_SECRET}`
+		);
+
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        })
+	};
 
     // Clear Users
     const clearUsers = () => dispatch({ type: CLEAR_USERS });
@@ -65,7 +77,8 @@ const GithubState = props => {
                 loading: state.loading,
                 searchUsers,
                 clearUsers,
-                getUser
+                getUser,
+                getUserRepos
 			}}
 		>
 			{props.children}
