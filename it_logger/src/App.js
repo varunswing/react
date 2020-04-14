ed434aa3b03c8d21de13646c6b0a18e7d09587ed
varunswing <varuns939@gmail.com>
2020-04-14 21:39:20 +0530

    Create App.js

---
+++ it_logger/src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+	useEffect(() => {
+		// Init Materialize JS
+		M.AutoInit();
+	});
+	return (
+		<Fragment>
+			<SearchBar />
+		</Fragment>
+	);
+};
+
+export default App;
