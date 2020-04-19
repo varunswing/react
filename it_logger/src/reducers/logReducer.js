db13a45dd768cae3cf91549ea1f8f243147368b8
varunswing <varuns939@gmail.com>
2020-04-19 22:54:37 +0530

    Update logReducer.js

--- it_logger/src/reducers/logReducer.js
+++ it_logger/src/reducers/logReducer.js
@@ -1,4 +1,14 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
+import {
+	GET_LOGS,
+	SET_LOADING,
+	LOGS_ERROR,
+	ADD_LOG,
+	DELETE_LOG,
+	UPDATE_LOG,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	SEARCH_LOGS
+} from '../actions/types';
 
 const initialState = {
 	logs: null,
@@ -14,6 +24,40 @@ export default (state = initialState, action) => {
 				...state,
 				logs: action.payload,
 				loading: false
+			};
+		case ADD_LOG:
+			return {
+				...state,
+				logs: [...state.logs, action.payload],
+				loading: false
+			};
+		case DELETE_LOG:
+			return {
+				...state,
+				logs: state.logs.filter(log => log.id !== action.payload),
+				loading: false
+			};
+		case UPDATE_LOG:
+			return {
+				...state,
+				logs: state.logs.map(log =>
+					log.id === action.payload.id ? action.payload : log
+				)
+			};
+		case SET_CURRENT:
+			return {
+				...state,
+				current: action.payload
+			};
+		case SEARCH_LOGS:
+			return {
+				...state,
+				logs: action.payload
+			};
+		case CLEAR_CURRENT:
+			return {
+				...state,
+				current: null
 			};
 		case SET_LOADING:
 			return {
