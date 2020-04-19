6794e6d1456f9f97c163e4a67ddfe0589b951508
varunswing <varuns939@gmail.com>
2020-04-19 22:57:11 +0530

    Update logActions.js

--- it_logger/src/actions/logActions.js
+++ it_logger/src/actions/logActions.js
@@ -1,4 +1,14 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import {
+	GET_LOGS,
+	SET_LOADING,
+	LOGS_ERROR,
+	ADD_LOG,
+	DELETE_LOG,
+	UPDATE_LOG,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+    SEARCH_LOGS
+} from './types';
 
 // export const getLogs = () => {
 // 	return async dispatch => {
@@ -14,6 +24,7 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 // 	};
 // };
 
+// Get logs from server
 export const getLogs = () => async dispatch => {
 	try {
 		setLoading();
@@ -22,13 +33,129 @@ export const getLogs = () => async dispatch => {
 		const data = await res.json();
 
 		dispatch({
-			types: GET_LOGS,
+			type: GET_LOGS,
 			payload: data
 		});
-	} catch {
+	} catch (err) {
 		dispatch({
-			types: GET_LOGS,
-			payload: data
+			type: LOGS_ERROR,
+			payload: err.response.data
 		});
 	}
+};
+
+// Add new log
+export const addLog = log => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch('/logs', {
+			method: 'POST',
+			body: JSON.stringify(log),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const data = await res.json();
+
+		dispatch({
+			type: ADD_LOG,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+};
+
+// Delete log from server
+export const deleteLog = id => async dispatch => {
+	try {
+		setLoading();
+
+		await fetch(`/logs/${id}`, {
+			method: 'DELETE'
+		});
+
+		dispatch({
+			type: DELETE_LOG,
+			payload: id
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+};
+
+// Update log from server
+export const updateLog = log => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs/${log.id}`, {
+			method: 'PUT',
+			body: JSON.stringify(log),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		const data = await res.json();
+
+		dispatch({
+			type: UPDATE_LOG,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+};
+
+// Search server logs
+export const searchLogs = text => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs?q=${text}`);
+		const data = await res.json();
+
+		dispatch({
+			type: SEARCH_LOGS,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+};
+
+// Set current log
+export const setCurrent = log => {
+	return {
+		type: SET_CURRENT,
+		payload: log
+	};
+};
+
+// Clear current log
+export const clearCurrent = () => {
+	return {
+		type: CLEAR_CURRENT
+	};
+};
+
+// Set loading to true
+export const setLoading = () => {
+	return {
+		type: SET_LOADING
+	};
 };
