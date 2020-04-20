927e53baafd06b1537b8dfa68576fcb8676dd2b5
varunswing <varuns939@gmail.com>
2020-04-20 20:43:32 +0530

    Update AddTechModal.js

--- it_logger/src/components/techs/AddTechModal.js
+++ it_logger/src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
 
@@ -9,7 +12,12 @@ const AddTechModal = () => {
 		if (firstName === '' || lastName === '') {
 			M.toast({ html: 'Please enter the first and last name' });
 		} else {
-			console.log(firstName, lastName);
+			addTech({
+				firstName,
+				lastName
+			});
+
+			M.toast({ html: `${firstName} ${lastName} was added as tech` });
 
 			// Clear Fields
 			setFirstName('');
@@ -27,7 +35,8 @@ const AddTechModal = () => {
 							type='text'
 							name='firstName'
 							value={firstName}
-							onChange={e => setFirstName(e.target.value)}						/>
+							onChange={e => setFirstName(e.target.value)}
+						/>
 						<label htmlFor='firstName' className='active'>
 							First Name
 						</label>
@@ -61,4 +70,8 @@ const AddTechModal = () => {
 	);
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+	addTech: PropTypes.func.isRequired
+};
+
+export default connect(null, { addTech })(AddTechModal);
